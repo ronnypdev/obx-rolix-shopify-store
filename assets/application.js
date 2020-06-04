f003d887b39656bace0848351b601ce1f1a2b91e
Ronny Perez <ronnypdev@gmail.com>
2020-06-03 22:07:45 -0400

    built the hamburger toggle button

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,8 @@
 // Put your applicaiton javascript here
+// toggle menu script
+const hamburgerButton = document.querySelector('#nav-toggle');
+
+hamburgerButton.addEventListener('click', function () {
+  console.log(this);
+  this.classList.toggle('active');
+});
